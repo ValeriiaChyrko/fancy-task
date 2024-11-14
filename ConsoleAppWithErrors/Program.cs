36e07d3bc9039842adb5cf8edf9275e23d671131
Valeriia <valeriia.chyrko@oa.edu.ua>
2024-11-14 15:20:26 +0200

    Add some errors.

--- ConsoleAppWithErrors/Program.cs
+++ ConsoleAppWithErrors/Program.cs
@@ -1,5 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace ExampleWithErrors
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
 
-//This is error in file
+            // Синтаксична помилка: пропущена дужка
+            Console.WriteLine("This is a test message";
+
+            // Логічна помилка: неправильне використання змінної
+            int result = AddNumbers(5, "10"); // Спроба передати рядок замість цілого числа
+
+            Console.WriteLine("Result: " + result);
+        }
+
+        // Метод, який додає два числа
+        static int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
