1930e1570dfa6c54d8d9159f67f30668a3fc1d82
Valeriia <valeriia.chyrko@oa.edu.ua>
2024-11-14 15:27:16 +0200

    Add warnings.

--- ConsoleAppWithErrors/Program.cs
+++ ConsoleAppWithErrors/Program.cs
@@ -1,26 +1,36 @@
 ﻿using System;
 
-namespace ExampleWithErrors
+namespace ExampleWithWarnings
 {
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
 
-            // Синтаксична помилка: пропущена дужка
-            Console.WriteLine("This is a test message";
+            // Попередження: невикористовувана змінна
+            int unusedVariable = 42;
 
-            // Логічна помилка: неправильне використання змінної
-            int result = AddNumbers(5, "10"); // Спроба передати рядок замість цілого числа
+            // Попередження: неявне приведення типів
+            double number = 10.5;
+            int result = AddNumbers(5, (int)number); // Явне приведення типу
 
             Console.WriteLine("Result: " + result);
+
+            // Попередження: метод не використовується
+            PrintMessage("This is a test message");
         }
 
         // Метод, який додає два числа
         static int AddNumbers(int a, int b)
         {
             return a + b;
+        }
+
+        // Метод, який виводить повідомлення
+        static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
         }
     }
 }
